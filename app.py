4a2f74e1945bdd714dc3a888403b0f8dce6b7f5f
Dhruvan Ganadhandayuthapani <dhruvan2006@gmail.com>
2024-06-12 23:56:52 +0200

    Added 3 transformations

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ with col1:
 df = pd.read_csv('btc-gli.csv')
 df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
 df.set_index('Date', inplace=True)
+# df = df[df.index >= pd.to_datetime('2022-11-15')]
 
 # Line chart
 fig = px.line(df, x=df.index, y=['BTC', 'GLI'])#, title='Bitcoin and GLI')
@@ -56,25 +57,49 @@ fig.update_layout(margin=dict(t=25, b=100))
 st.plotly_chart(fig)
 
 # Polynomial Regression
-col1, col2 = st.columns(2)
+st.subheader('Polynomial Regression')
+
+col1, col2, col3 = st.columns(3)
 with col1:
-    st.subheader('Polynomial Regression')
-with col2:
     degree = st.number_input('Enter the degree of the polynomial', min_value=1, max_value=10, value=1, step=1)
 
-poly_features = np.vander(df['GLI'], N=degree+1, increasing=True)
+transformations = ('None', 'Log', 'Square Root', 'Reciprocal')
+with col2:
+    btc_transformation = st.selectbox('Select the transformation for BTC', transformations)
+with col3:
+    gli_transformation = st.selectbox('Select the transformation for GLI', transformations)
 
-model = sm.OLS(df['BTC'], poly_features)
+if btc_transformation == 'Log':
+    transformed_btc = np.log(df['BTC'])
+elif btc_transformation == 'Square Root':
+    transformed_btc = np.sqrt(df['BTC'])
+elif btc_transformation == 'Reciprocal':
+    transformed_btc = np.reciprocal(df['BTC'])
+else:
+    transformed_btc = df['BTC']
+
+if gli_transformation == 'Log':
+    transformed_gli = np.log(df['GLI'])
+elif gli_transformation == 'Square Root':
+    transformed_gli = np.sqrt(df['GLI'])
+elif gli_transformation == 'Reciprocal':
+    transformed_gli = np.reciprocal(df['GLI'])
+else:
+    transformed_gli = df['GLI']
+
+poly_features = np.vander(transformed_gli, N=degree+1, increasing=True)
+
+model = sm.OLS(transformed_btc, poly_features)
 results = model.fit()
 
 stddev = np.std(results.resid)
 
-fig = px.scatter(x=df['GLI'], y=df['BTC'])
+fig = px.scatter(x=transformed_gli, y=transformed_btc)
 fig.update_traces(marker=dict(color='orange'))
 fig.update_xaxes(title_text='GLI')
 fig.update_yaxes(title_text='BTC')
 
-x_range = np.linspace(df['GLI'].min(), df['GLI'].max(), 100)
+x_range = np.linspace(transformed_gli.min(), transformed_gli.max(), 100)
 x_poly_range = np.vander(x_range, N=degree+1, increasing=True)
 y_range = results.predict(x_poly_range)
 
@@ -84,7 +109,7 @@ fig.add_scatter(x=x_range, y=y_range - stddev, mode='lines', line=dict(dash='das
 fig.update_layout(showlegend=False)
 
 fair_value = results.predict(poly_features)
-fig2 = px.line(x=df.index, y=df['BTC'])
+fig2 = px.line(x=df.index, y=transformed_btc)
 fig2.update_traces(line_color='orange')
 fig2.add_scatter(x=df.index, y=fair_value, mode='lines', line=dict(color='white'))
 fig2.add_scatter(x=df.index, y=fair_value + stddev, mode='lines', line=dict(dash='dash', color='red'))
